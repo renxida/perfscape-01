98d53e4d52810ce8bcb47b6f96256cb5734b87f3
Xida Ren <cedar.ren@gmail.com>
2022-01-15 16:24:33 -0500

    begin adaptation to ARM

--- scripts/00_all_setup.sh
+++ scripts/00_all_setup.sh
@@ -3,7 +3,7 @@ do
     echo Setting up $target
     ssh root@$target 'apt update'
     ssh root@$target 'apt install -y linux-tools-`uname -r`'
-    ssh root@$target 'echo 0 > sudo tee /proc/sys/kernel/nmi_watchdog'
+    ssh root@$target 'echo 0 | sudo tee /proc/sys/kernel/nmi_watchdog'
     echo Done setting up $target
 done
 exit
